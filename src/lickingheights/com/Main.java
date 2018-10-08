0ea6a62ff40f1acb16c0c84bb6d3c764a8430d9e
Anusha Ghimirey <anusha.ghimirey.2022@student.lhschools.org>
2018-10-08 08:04:43 -0400

    Almost done

--- src/lickingheights/com/Main.java
+++ src/lickingheights/com/Main.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// initializing
-       int pickedNumber;
-       pickedNumber = (int)Math.random();
+        // initializing
+        int pickedNumber;
+        pickedNumber = (int) (Math.random() * 100);
         Scanner keyboard;
         keyboard = new Scanner(System.in);
         String consent;
@@ -16,24 +16,40 @@ public class Main {
 
         System.out.println("Would you like to play the number guessing game?");
         consent = keyboard.nextLine();
-        System.out.println("Alright then lets begin");
+        System.out.println("Great! I'll explain the rules");
         System.out.println("There will be a randomly generated number and you will try to guess what it is");
-        System.out.println("Okay go ahead and guess the number");
+        System.out.println("You will only get 3 tries to guess the number");
+        System.out.println("Are you ready to begin?");
+        consent = keyboard.nextLine();
+        System.out.println("You can go ahead and guess the number now. Good luck!");
         number = keyboard.nextInt();
-        if (pickedNumber<number)
+
+        if (pickedNumber < number)
             System.out.println("That number is too big try again");
-        else if (pickedNumber>number)
+        else if (pickedNumber > number)
             System.out.println("That number is too small try again");
         else if (pickedNumber == number)
             System.out.println("Congrats you guessed correctly!");
-        System.out.println("Try to guess the number again");
+
         number = keyboard.nextInt();
-        if (pickedNumber<number)
+        if (pickedNumber < number)
             System.out.println("That number is too big try again");
-        else if (pickedNumber>number)
+        else if (pickedNumber > number)
             System.out.println("That number is too small try again");
         else if (pickedNumber == number)
             System.out.println("Congrats you guessed correctly!");
+
+        number = keyboard.nextInt();
+        if (pickedNumber < number)
+            System.out.println("That number is too big :( Good try though maybe you will do better next time.");
+        else if (pickedNumber > number)
+            System.out.println("That number is too small :( Good try though, maybe you will do better next time.");
+        else if (pickedNumber == number)
+            System.out.println("Congrats you guessed correctly!");
+
+
+
+
 
 
 
